8b5c6eabccf0bb9074deb823420b65727c32deb4
Joachim <jj@jj22.de>
2020-05-18 01:18:34 +0200

    asd

---
+++ allMaven/src/main/uni/pvs/blatt04/Main.java
@@ -0,0 +1,12 @@
+package de.jj22.uni.pvs.blatt04;
+
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner1 = new Scanner(new FileInputStream("bigInput.txt"));
+        scanner1.nextInt();
+    }
+}
---
+++ allMaven/src/test/uni/pvs/blatt04/Test.java
@@ -0,0 +1,20 @@
+package de.jj22.uni.pvs.blatt04;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
