cc3d00ec1bc8a84aa367ad189a789db64ed088fa
Joachim <jj@jj22.de>
2020-05-18 01:13:20 +0200

    asd

---
+++ allMaven/src/main/uni/pvs/blatt04/App.java
@@ -0,0 +1,13 @@
+package uni.pvs.blatt04;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
