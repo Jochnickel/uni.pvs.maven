00886d30a969ab7b08da3e8c9765946099d8a223
Joachim Janke <jj17@wsl50.informatik.uni-ulm.de>
2020-05-18 12:34:31 +0200

    asd

--- allMaven/src/main/java/de/jj22/uni/pvs/blatt04/Main.java
+++ allMaven/src/main/java/de/jj22/uni/pvs/blatt04/Main.java
@@ -1,41 +1,61 @@
 package de.jj22.uni.pvs.blatt04;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.function.ToLongFunction;
 
-public class Main 
-{
-	public static void main( String[] args )
-	{
-		String fn = "bigInput.txt";
-		ToLongFunction<String> foo1 = (filename) -> {calcSumFromFileLines1(filename);};
-		final long time1 = getExecutionTime(()->{calcSumFromFileLines1()}, args);
+public class Main {
+	public static void main(String[] args) {
+		final long[] time1 = getExecutionTime(filename -> {
+			try {
+				return calcSumFromFileLines1(filename);
+			} catch (Exception e) {
+				throw new Error(e);
+			}
+		}, "bigInput.txt");
+		final long[] time2 = getExecutionTime(filename -> {
+			try {
+				return calcSumFromFileLines2(filename);
+			} catch (Exception e) {
+				throw new Error(e);
+			}
+		}, "bigInput.txt");
+		System.out.format("Calculated %d in %d seconds with Scanner\n", time1[1], time1[0]);
+		System.out.format("Calculated %d in %d seconds with String to Long\n", time2[1], time2[0]);
 	}
 
-	public static long getExecutionTime(Consumer f, Object... args) {
+	/*
+	* @return ExecutionTime, Result
+	*/
+	public static long[] getExecutionTime(ToLongFunction<String> strToLongFunc, String str) {
 		final long startTime = System.currentTimeMillis();
-		f.accept(args);
-		return System.currentTimeMillis() - startTime;
+		final long result = strToLongFunc.applyAsLong(str);
+		final long execTime = System.currentTimeMillis() - startTime;
+		return new long[] { execTime, result };
 	}
 
-	public static long calcSumFromFileLines1(String filename){
+	public static long calcSumFromFileLines1(String filename) throws FileNotFoundException {
 		Scanner sc = new Scanner(new FileInputStream(filename));
 		long sum = 0;
-		while (sc.hasNextInt()){
+		while (sc.hasNextInt()) {
 			sum += sc.nextInt();
 		}
 		return sum;
 	}
 
-	public static long calcSumFromFileLines2(String filename){
-		BufferedReader br = new BufferedReader(new FileInputStream(filename));
+	public static long calcSumFromFileLines2(String filename) throws FileNotFoundException, IOException {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
 		long sum = 0;
-		while (br.ready()){
+		while (br.ready()) {
 			sum += Integer.parseInt(br.readLine());
 		}
+		br.close();
 		return sum;
 	}
 }
