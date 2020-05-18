3010a73b98943ca929e4d5f7cc164c07a80276dc
Joachim Janke <jj17@wsl50.informatik.uni-ulm.de>
2020-05-18 15:43:57 +0200

    asd

--- sokoban/src/main/java/de/uulm/sp/pvs/App.java
+++ sokoban/src/main/java/de/uulm/sp/pvs/App.java
@@ -1,15 +1,24 @@
 package de.uulm.sp.pvs;
 
+import java.util.Scanner;
+
 import de.uulm.sp.pvs.util.Sokoban;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Sokoban game = new Sokoban();
-    }
+public class App {
+	public static void main(String[] args) {
+		char[][] board = SokobanBoards.getDefaultBoard();
+
+		System.out.print("\nStarting game! ");
+		{ Scanner scanner = new Scanner(System.in);
+			printBoardAndAskForInput(board);
+			for (String stdInput = scanner.next(); null != stdInput; stdInput = scanner.next()) {
+				System.out.println(stdInput);
+				printBoardAndAskForInput(board);
+			}
+		scanner.close();}
+	}
+
+	static void printBoardAndAskForInput(char[][] board){
+		System.out.printf("Current Board:\n\n%s\nInput: ", Sokoban.sokobanToString(board));
+	}
 }
