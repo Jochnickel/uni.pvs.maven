ee10703ebdda8dbec61d9355b8aca6695f9d495c
Joachim Janke <jj17@wsl50.informatik.uni-ulm.de>
2020-05-26 23:29:11 +0200

    asd

--- sokoban/src/main/java/de/uulm/sp/pvs/sokoban/SokobanLevel.java
+++ sokoban/src/main/java/de/uulm/sp/pvs/sokoban/SokobanLevel.java
@@ -1,17 +1,25 @@
 package de.uulm.sp.pvs.sokoban;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class SokobanLevel {
-    
+    FileInputStream xsdFile = new FileInputStream("sokoban.xsd");
+
     final char[][] board;
     final String name;
     Difficulty difficulty;
 
-    SokobanLevel(String pathToXML) {
-        
+    SokobanLevel(String pathToXML) throws FileNotFoundException, InvalidFileException {
+        if (isValidXML(pathToXML)){}
     }
 
-    static boolean isValidXML(String pathToXML, String pathToXSD){
+    static boolean isValidXML(String pathToXML, String pathToXSD) throws FileNotFoundException {
         
         return false;
+    }
+
+    public class InvalidFileException extends Exception {
+        private static final long serialVersionUID = 1L;
     }
 }
