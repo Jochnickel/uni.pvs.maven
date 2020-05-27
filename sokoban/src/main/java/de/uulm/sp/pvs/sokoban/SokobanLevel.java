d998ab03a3cb459ccc727f75dc9529eb0c95ed0b
Joachim Janke <jj17@wsl50.informatik.uni-ulm.de>
2020-05-27 14:53:58 +0200

    qwe

--- sokoban/src/main/java/de/uulm/sp/pvs/sokoban/SokobanLevel.java
+++ sokoban/src/main/java/de/uulm/sp/pvs/sokoban/SokobanLevel.java
@@ -1,39 +1,37 @@
 package de.uulm.sp.pvs.sokoban;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-import org.w3c.dom.ls.LSResourceResolver;
-import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 
 public class SokobanLevel {
     private static String xsdFileName = "sokoban.xsd";
     private static Validator validator = null;
 
-    final char[][] board;
-    final String name;
+    List<String> authors = new ArrayList<String>();
+    char[][] board;
+    String name;
     Difficulty difficulty;
 
     public SokobanLevel(String pathToXML) throws FileNotFoundException, InvalidFileException, Exception {
         if (isValidXML(pathToXML)) {
             var domRoot = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder().parse(pathToXML).getDocumentElement(); // can throw 3 exceptions
             var children = domRoot.getChildNodes();
-            for (int i = children.getLength()-1; 0 <= i; i--) {
-                var child = children.item(i);
-                if("Difficulty".equals(child.getNodeName()){
-                    switch (child.getNodeValue()) {
+            for (int iNode = children.getLength()-1; 0 < iNode; iNode--) {
+                var child = children.item(iNode);
+                if("Difficulty".equals(child.getNodeName())) {
+                    switch (child.getTextContent()) {
                         case "EASY":
                             difficulty = Difficulty.EASY;
                             break;
@@ -47,17 +45,29 @@ public class SokobanLevel {
                             difficulty = Difficulty.IMPOSSIBLE;
                             break;
                         default:
-                            throw new InvalidFileException("Parsing difficulty went wrong.")
+                            throw new InvalidFileException("Parsing difficulty went wrong.");
                     }
                 } else if ("LevelName".equals(child.getNodeName())){
                     this.name = child.getNodeValue();
                 } else if ("LevelData".equals(child.getNodeName())){
-                    
+                    var attrs = child.getAttributes();
+                    var width = Integer.parseInt(attrs.getNamedItem("width").getNodeValue());
+                    var height = Integer.parseInt(attrs.getNamedItem("height").getNodeValue());
+                    var levelString = child.getTextContent().replace("\n", "");
+                    try{
+                        board = new char[width][height];
+                        for (int i = 0, w = 0; w < board.length; w++) {
+                            for (int h = 0; h < board.length; h++) {
+                                board[w][h] = levelString.charAt(i);
+                            }
+                        }
+                    } catch (IndexOutOfBoundsException e){
+                        System.err.println("Board height/width corrupted");
+                    }
+                } else if ("Author".equals(child.getNodeName())){
+                    authors.add(child.getTextContent());
                 }
             }
-            board = null;
-            name = null;
-            difficulty = null;
         } else {
             throw new InvalidFileException();
         }
--- sokoban/src/test/java/de/uulm/sp/pvs/SokobanLevelTest.java
+++ sokoban/src/test/java/de/uulm/sp/pvs/SokobanLevelTest.java
@@ -6,8 +6,7 @@ import de.uulm.sp.pvs.sokoban.SokobanLevel;
 import de.uulm.sp.pvs.sokoban.SokobanLevel.*;
 
 public class SokobanLevelTest {
-    public static void main(String[] args) throws FileNotFoundException, InvalidFileException {
+    public static void main(String[] args) throws FileNotFoundException, InvalidFileException, Exception {
         SokobanLevel level = new SokobanLevel("test_level.xml");
-        SokobanLevel.isValidXML("test_level.xml");
     }
 }
