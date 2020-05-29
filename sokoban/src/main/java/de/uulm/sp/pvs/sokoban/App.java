25b4579e3d096c3ab8b0ded74af5025ec41ccfd5
Joachim Janke <jj17@wsl50.informatik.uni-ulm.de>
2020-05-29 17:41:48 +0200

    asd

--- sokoban/src/main/java/de/uulm/sp/pvs/sokoban/App.java
+++ sokoban/src/main/java/de/uulm/sp/pvs/sokoban/App.java
@@ -2,61 +2,63 @@ package de.uulm.sp.pvs.sokoban;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.util.Scanner;
+
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
 import de.uulm.sp.pvs.util.Sokoban;
 
 public class App {
 
-	private final static String[] WASD = { "W", "A", "S", "D" };
-	private final static String[] NWSE = { "N", "W", "S", "E" };
-	private final static String EXIT_KEY = "X";
-	private final static String TOGGLE = " ";
+	private final static char[] WASD = { 'W', 'A', 'S', 'D' };
+	private final static char[] NWSE = { 'N', 'W', 'S', 'E' };
+	private final static char EXIT_KEY = 'X';
+	private final static char TOGGLE_KEY = ' ';
 	/**
 	 * 4 letters to control the player, counterclockwise, starting with up.
 	 */
-	private static String[] movementKeys = NWSE;
+	private static char[] movementKeys = NWSE;
 
 	public static void main(String[] args) throws IOException {
-		final var board = SokobanBoards.getDefaultBoard();
-		System.out.print("\nStarting game! ");
+		char[][] board = SokobanBoards.getDefaultBoard();
+		playLevel(board);
+	}
+
+	static void playLevel(final char[][] board) throws IOException {
+		Terminal terminal = TerminalBuilder.terminal();
+		terminal.enterRawMode();
+		var reader = terminal.reader();
 		printBoardAndAskForInput(board);
-		{
-			Scanner scanner = new Scanner(System.in);
-			scanner.useDelimiter("");
-			for (String stdInput = String.valueOf(scanner.next().charAt(0));; stdInput = scanner.next()) {
-				try {
-					moveByKeyboardInt(stdInput, board);
-					printBoardAndAskForInput(board);
-				} catch (Exception e) {
-					switch (stdInput) {
-						case "\n":
-							break;
-						case EXIT_KEY:
-							scanner.close();
-							System.out.println("Bye");
-							System.exit(0);
-						case TOGGLE:
-							toggleControls();
-							break;
-						default:
-							System.err.print("Invalid input. ");
-					}
-					printAskForInput();
-				}
+		for (char read = Character.toUpperCase((char) reader.read());; read = Character
+				.toUpperCase((char) reader.read())) {
+			System.out.println(read);
+			if (EXIT_KEY == read) {
+				return;
 			}
+			if (TOGGLE_KEY == read) {
+				toggleControls();
+				printAskForInput();
+				continue;
+			}
+			try {
+				moveByChar(read, board);
+			} catch (IllegalArgumentException e) {
+				System.out.print("Invalid Input. ");
+				printAskForInput();
+				continue;
+			}
+			printBoardAndAskForInput(board);
 		}
 	}
 
-	static void moveByKeyboardInt(final String keyboardInputAnyCase, char[][] board) throws IllegalArgumentException {
-		final String keyboardInput = keyboardInputAnyCase.toUpperCase();
-		if (Objects.equals(keyboardInput, movementKeys[0])) {
+	static void moveByChar(final char keyboardInputUpperCase, char[][] board) throws IllegalArgumentException {
+		if (Objects.equals(keyboardInputUpperCase, movementKeys[0])) {
 			Sokoban.moveNorth(board);
-		} else if (Objects.equals(keyboardInput, movementKeys[1])) {
+		} else if (Objects.equals(keyboardInputUpperCase, movementKeys[1])) {
 			Sokoban.moveWest(board);
-		} else if (Objects.equals(keyboardInput, movementKeys[2])) {
+		} else if (Objects.equals(keyboardInputUpperCase, movementKeys[2])) {
 			Sokoban.moveSouth(board);
-		} else if (Objects.equals(keyboardInput, movementKeys[3])) {
+		} else if (Objects.equals(keyboardInputUpperCase, movementKeys[3])) {
 			Sokoban.moveEast(board);
 		} else {
 			throw new IllegalArgumentException();
@@ -84,12 +86,14 @@ public class App {
 	}
 
 	static void printBoardAndAskForInput(char[][] board) {
+		System.out.print("\033[H\033[2J"); 
+		System.out.flush();
 		System.out.printf("Current Board:\n\n%s\n", Sokoban.sokobanToString(board));
 		printAskForInput();
 	}
 
 	static void printAskForInput() {
-		System.out.printf("Where do you want to go? (%s/%s/%s/%s or X to exit): ", movementKeys[0], movementKeys[1],
-				movementKeys[2], movementKeys[3]);
+		System.out.printf("Where do you want to go? (%s/%s/%s/%s or %c to exit): ", movementKeys[0], movementKeys[1],
+				movementKeys[2], movementKeys[3], EXIT_KEY);
 	}
 }
