8722b480ddbbce272d4ac02e5320b1c5c0c40d03
JJ <jj@jj22.de>
2020-06-23 13:53:01 +0000

    asd

--- sokoban/src/main/java/de/uulm/sp/pvs/util/SokobanUtils.java
+++ sokoban/src/main/java/de/uulm/sp/pvs/util/SokobanUtils.java
@@ -1,26 +1,20 @@
 package de.uulm.sp.pvs.util;
 
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileSystems;
-import java.nio.file.Path;
+import java.nio.file.Files;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
 
+import de.uulm.sp.pvs.sokoban.InvalidFileException;
 import de.uulm.sp.pvs.sokoban.SokobanLevel;
 
 public class SokobanUtils {
-	public static void main(String[] args) throws IOException {
-		// loadLevels("/home/jj17/pvs/Blatt09/");
-		final Path startingDir = Paths.get(args[0]);
-		PathMatcher matcher =
-    FileSystems.getDefault().getPathMatcher("glob:*.{java,class}");
-
-	Path filename = startingDir
-	if (matcher.matches(filename)) {
-		System.out.println(filename);
-	}
+	public static void main(final String[] args) throws Exception {
+		loadLevels("/mnt/d/Uni/pvs/Blatt09");
 	}
 
 	/**
@@ -28,24 +22,24 @@ public class SokobanUtils {
 	 * @param dirPath folder with multiple Sokoban_level.xml files
 	 * @return List<> of created Sokobanlevels
 	 */
-	public static List<SokobanLevel> loadLevels(String dirPath) {
-
-		File folder = new File(dirPath);
-		File[] listOfFiles = folder.listFiles();
-
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) {
-				System.out.println("f      " + listOfFiles[i].getName());
-			} else if (listOfFiles[i].isDirectory()) {
-				System.out.println("d      " + listOfFiles[i].getName());
-			}
+	public static List<SokobanLevel> loadLevels(final String dirPath)
+			throws FileNotFoundException, InvalidFileException {
+		final var asd = Paths.get(dirPath);
+		if (Files.notExists(asd)) {
+			throw new FileNotFoundException();
 		}
-
-		matcher = FileSystems.getDefault()
-                    .getPathMatcher("glob:*.txt");
-		final fileName = startingDir.getFileName();
-		if (null!=	fileName &&  mat)
-		
-		return null;
+		final List<SokobanLevel> list = new LinkedList<SokobanLevel>();
+		try {
+			Files.list(asd).filter(a -> a.toString().endsWith(".xml")).forEach(fName -> {
+				try {
+					list.add(new SokobanLevel(fName.toString()));
+				} catch (FileNotFoundException | InvalidFileException e) {
+					System.out.printf("Didn't add %s\n", fName.toString());
+				}
+			});
+		} catch (IOException e) {
+			throw new InvalidFileException("This Path does not lead to a directory");
+		}
+		return list;
 	}
 }
