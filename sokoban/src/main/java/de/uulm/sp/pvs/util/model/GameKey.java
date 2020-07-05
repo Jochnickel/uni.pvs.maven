a7c7ae4717b73750576c1f963d38e906f6f09938
Joachim Janke <jj17@wsl51.informatik.uni-ulm.de>
2020-07-05 21:42:10 +0200

    asd

--- sokoban/src/main/java/de/uulm/sp/pvs/util/model/GameKey.java
+++ sokoban/src/main/java/de/uulm/sp/pvs/util/model/GameKey.java
@@ -1,7 +1,10 @@
 package de.uulm.sp.pvs.util.model;
 
+import java.sql.Timestamp;
+
 @SuppressWarnings("unused")
 public class GameKey {
     private String level_name;
     private int player_id;
+    private Timestamp date;
 }
